e8da73b55c6cbc25d60d3597f2d4efa406fcb7b7
Deepak H <deepakh94@gmail.com>
2019-02-03 19:29:43 -0500

    User Interface with Landing Page integrated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Projects from '../src/containers/addProjects/addProjects'
+import Search from "./components/searchbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Projects></Projects>
-      </div>
+      <Router>
+      <React.Fragment>
+       <Route exact path="/" component={Search} />
+       <Route exact path="/addprojects" component={Projects} />
+       </React.Fragment>
+      </Router>
     );
   }
 }
---
+++ src/components/searchbar.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import classes from './searchbar.css';
+
+
+class searchbar extends Component {
+  state = {
+    searchString: "",
+    postfromUser: [],
+    postfromProjects: []
+  }
+  componentDidMount() {
+    axios.get('http://127.0.0.1:5000/find/')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          postfromUser: res.data.ListfromUsers , //can use .slice method to get only a few
+          postfromProjects: res.data.ListfromProjects
+        })
+      })
+  }
+  SearchchangeHandler = (e) => {
+    this.setState({
+      searchString: e.target.value
+    })
+  }
+  handleclick = (e) => {
+    console.log(this.state.searchString);
+    axios.get('http://127.0.0.1:5000/find/?ftext='+ this.state.searchString)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          postfromProjects: res.data.ListfromProjects,  //can use .slice method to get only a few
+          postfromUser: res.data.ListfromUsers
+        })
+      });
+
+    e.preventDefault();
+  }
+
+  render() {
+    const { postfromProjects }=this.state;
+       const postList = postfromProjects.length ? (
+        postfromProjects.map(post => {
+               return(
+                   <div className={classes.Book} id={post._id}>
+                   <p>{post.projectName}</p>
+                   <p>{post.companyName}</p>
+                   <p>{post.numberOfPpl}</p>
+                   <p>{post.payPerHour}</p>
+                   <p>{post.projectDescription}</p>
+                   <p>{post.projectLocation}</p>
+                   <p>{post.projectDepartment}</p>
+                   </div>
+               )}))
+        :(
+           <div className="center"> No projects to be displayed </div>
+       );
+       
+       const { postfromUser }=this.state;
+       const postList1 = postfromUser.length ? (
+        postfromUser.map(post => {
+               return(
+                   <div className={classes.Book} id={post._id}>
+                   <p>{post.Name}</p>
+                   <p>{post.Age}</p>
+                   </div>
+               )}))
+        :(
+           <div className="center"> No Freelancers to be displayed </div>
+       );
+    
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <h1>9to5IT</h1>
+          <form onSubmit={this.handleclick}>
+            <div className="Search">
+              <div style={{ display: "flex" }}>
+                <TextField
+                  onChange={this.SearchchangeHandler}
+                  id="outlined-full-width"
+                  label="Search"
+                  style={{ margin: 8 }}
+                  placeholder="Enter the name of Organization/Coder"
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                />
+              </div>
+            </div>
+            <div className={classes.Button}>
+              <Button
+                style={{
+                  backgroundColor: "#424242"
+                }}
+                onClick={this.handleclick}
+                variant="contained"
+                color="primary">
+                Search
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <h4 className="center">PROJECTS</h4>
+          <div className={classes.Disp}>
+           {postList}
+          </div>
+          <h4> FREE LANCERS </h4>
+          {postList1}
+        </div>
+      </div>
+
+    )
+  }
+
+}
+export default searchbar;
