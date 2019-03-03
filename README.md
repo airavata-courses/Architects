6b58c7f4933e4d5f7b25806c681045d403bbe409
Deepak H <deepakh94@gmail.com>
2019-03-03 17:13:31 -0500

    Search_Filterwith zookeeper

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-# Architects
-
-Spring 2019 Project Team
-
-
-![Project Wiki for 9to5IT - A Free Lancers Portal](https://github.com/airavata-courses/Architects/blob/master/9to5IT.png)
-
-# Milestone 1 :
-
-https://github.com/airavata-courses/Architects/wiki/9to5IT-:-Milestone-1
-
-# Milestone 1 :
-
-https://github.com/airavata-courses/Architects/wiki/9T05IT:-Milestone-2
- 
+# -Architects
---
+++ ZooKeeperHandler.py
@@ -0,0 +1,21 @@
+from kazoo.client import KazooClient
+from kazoo.client import KazooState
+from kazoo.client import KeeperState
+from kazoo.exceptions import NodeExistsError, NoNodeError, ConnectionLossException
+from flask import request, jsonify
+from json import load
+import requests
+import json
+
+# Zookeeper Service Registry
+class ZookeeperHandler:
+    def registerAuthService(self,host,port):
+        zk = KazooClient(hosts=host+":"+"2181", read_only=True)
+        zk.start()
+        path = '/ensemble/find'
+        host= str(requests.get('https://ip.42.pl/raw').text) # AuthServices
+        pass_data="192.168.1.127"+":"+port
+        try:
+            zk.create(path,value=pass_data, ephemeral=True, makepath=True)
+        except NodeExistsError:
+            print("Node already exists")
