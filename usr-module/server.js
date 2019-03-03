b0b3f4b20e1c5a594af76ec08063f1920fef9b19
hsadi <hsadi@iu.edu>
2019-03-03 16:08:05 -0500

    #83 zookeeper for usr-mgmt

--- usr-module/server.js
+++ usr-module/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const ZooKeeper = require('node-zookeeper-client');
 const register = require("./routes/api/register");
 const getuser=require("./routes/api/getuser");
+const ip = require("ip");
 
 const app=express();
 app.use(bodyParser.json());
 app.use(cors());
+
+
 
 const db = require("./config/keys").mongoURI;
 mongoose
@@ -25,3 +29,21 @@ mongoose
   const port= process.env.PORT || 22000
 
   app.listen(port, () => console.log(`Server started on port ${port}`));
+
+  const client = ZooKeeper.createClient('149.165.171.39:2181',{ sessionTimeout: 5000 });
+ // console.log("here")
+ console.log(client)
+  client.once('connected', function () {
+    console.log('Connected to the server.');
+ 
+    client.create("/ensemble/userManagement",new Buffer(ip.address() +":"+port),ZooKeeper.CreateMode.EPHEMERAL, function (error) {
+        if (error) {
+            console.log('Failed to create node: %s due to: %s.', error);
+        } else {
+            console.log('Node: %s is successfully created.');
+        }
+ 
+        //client.close();
+    });
+});
+client.connect();
