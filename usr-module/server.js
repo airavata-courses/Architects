9aa713fb9ea5eafeb6782f1738d078a42d471444
Deepak Hanumanthaiah <41876063+deepakh2810@users.noreply.github.com>
2019-03-02 19:01:46 -0500

    Update server.js

--- usr-module/server.js
+++ usr-module/server.js
@@ -22,6 +22,6 @@ mongoose
   app.use("/getuser", getuser);
 
 
-  const port= process.env.PORT || 22000
+  const port= process.env.PORT || 5000
 
   app.listen(port, () => console.log(`Server started on port ${port}`));
