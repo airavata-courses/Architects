371f07c14dacbf8f0b746fb7ec3ee4d41e00e3ba
hsadi <hsadi@iu.edu>
2019-03-03 19:49:44 -0500

    temp fix for login authenticator

--- api-server/auth-server/routes/loginAuthenticator.js
+++ api-server/auth-server/routes/loginAuthenticator.js
@@ -9,53 +9,78 @@ const register = require("../../static/Registry.json");
 
 router.post("/", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const email = req.body.email;
-  const password = req.body.password;
 
-    console.log(req.body.email);
-  Axios.get(register.userModule+ register.services.route.getUserForAuth+"/"+ req.body.email).then(login => {
-    console.log(login.data);
-  if (!login.data) {
-    console.log(login);
-    errors.data = "User email not found";
-    return res.status(404).json(errors);
-  }
-  bcrypt.compare(password, login.data.password).then(isMatch => {
-    if (isMatch) {
-      //user matched
-      const payload = {
-        FirstName: login.data.FirstName,
-        Email: login.data.email
-      };
+
+  const zookeeperData=zkObject.znodeData;
+  const zooKeeperExists=zkObject.znodeExists;
+  console.log("User Management !! connecting string ");
+  let connectionString
+  zooKeeperExists("/ensemble/userManagement")
+  .then(doesExist=>{
+    return zookeeperData("/ensemble/userManagement")
+  })
+  .then(data=>{
+    connectionString="http://"+data
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+  
+      console.log(req.body.email);
+    Axios.get(register.userModule+ register.services.route.getUserForAuth+"/"+ req.body.email).then(login => {
+      console.log(login.data);
+    if (!login.data) {
+      console.log(login);
+      errors.data = "User email not found";
+      return res.status(404).json(errors);
+    }
+    bcrypt.compare(password, login.data.password).then(isMatch => {
+      if (isMatch) {
+        //user matched
+        const payload = {
+          FirstName: login.data.FirstName,
+          Email: login.data.email
+        };
+        
+          //sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 36000 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
+        }
       
-        //sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 36000 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      }
-    
-       else {
-        errors.data = "Password incorrect";
-         return res.status(400).json(errors);
-       }
+         else {
+          errors.data = "Password incorrect";
+           return res.status(400).json(errors);
+         }
+      });
+    })
+    .catch(error => {
+      errors.data="Email Incorrect";
+      return res.status(400).json(errors);
+      //console.log(error)
     });
   })
-  .catch(error => {
-    errors.data="Email Incorrect";
-    return res.status(400).json(errors);
-    //console.log(error)
+  .catch(error=>{
+    return res.status(400).json(error);
   });
+
+
+
+
+
+
+
+
+
 });
 
 module.exports = router;
